7506f058182e4315afcf09c8ebf1817035886e2f
= <=>
2023-07-30 19:33:49 +0530

    initail commit created download react-router-dom and setup navigation

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Hero from "./pages/app/hero/Hero";
+import Nav from "./components/nav/Nav";
+import Courses from "./pages/app/courses/Courses";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+
+function App() {
+  let router = createBrowserRouter([
+    { path:'/',
+      element:<Nav/>,
+      children:[
+        { 
+          index:true,
+          element:<Hero/>
+        },
+        {
+          path:'/courses',
+          element:<Courses/>
+        }
+      ]
+    },
+  ])
+  return (
+    <div className="App">
+      <RouterProvider router={router}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,33 @@
+import style from "./Nav.module.css";
+import { Outlet,NavLink, Link } from "react-router-dom";
+function Nav() {
+ 
+  return (
+    <>
+      <nav>
+        <div className={style.nav_container}>
+          <div className={style.nav_title_wrapper}>
+            <Link to={'/'}>
+              <img
+                className={style.logo}
+                src="https://files.codingninjas.in/pl-ninja-16706.svg"
+                alt="logo"
+              />
+            </Link>
+            <h4>Coding Ninjas</h4>
+          </div>
+          <div className={style.nav_details}>
+            <button>
+              <NavLink to={'/courses'}>
+                {({isActive}) => (isActive ? "OnCourses":"Courses")}
+              </NavLink> 
+            </button>
+          </div>
+        </div>
+      </nav>
+      <Outlet/>
+    </>
+  );
+}
+
+export default Nav;
